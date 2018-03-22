faefd9344b8e8af9c93f57e10eabf69f4b176a2e
Charles Ferentchak <ferentchak@gmail.com>
2018-03-21 22:39:44 -0600

    better docs support

--- moveDocs.sh
+++ moveDocs.sh
@@ -2,7 +2,7 @@ eslint *.ts --fix > /dev/null&
 eslint built/*.js --fix > /dev/null&
 git submodule init
 git submodule update
-typedoc -out tempDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules
+node_modules/.bin/typedoc -out tempDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules --theme markdown
 mv docs/.git tempDocs/
 rm -rf docs/
 mv  tempDocs/ docs/
