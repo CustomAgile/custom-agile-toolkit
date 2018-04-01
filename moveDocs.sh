00f221d83fa690decfbacb4c5bbc290d6522d507
Charles Ferentchak <ferentchak@gmail.com>
2018-04-01 15:16:13 -0600

    removing bad command arg

--- moveDocs.sh
+++ moveDocs.sh
@@ -5,5 +5,5 @@ ts-node ClassGenerator.ts> /dev/null
 node_modules/.bin/typedoc --theme minimal -out tempDocs --tsconfig docsTsConfig.jsonc --ignoreCompilerErrors --mode modules --includeDeclarations --excludeExternals
 rm -rf docs/
 mv  tempDocs/ docs/
-node_modules/.bin/typedoc -out tempMarkdownDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules --theme markdown --includeDeclarations --excludeExternals --entryPoint \"RallyClient\"
+node_modules/.bin/typedoc -out tempMarkdownDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules --theme markdown --includeDeclarations --excludeExternals
 mv  tempMarkdownDocs/ docs/markdown/
