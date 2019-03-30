915fe9c15647480fcf7b375ba60b72a367021d5c
Charles Ferentchak <charles@customagile.com>
2019-03-30 18:27:31 -0500

    0.3.2

--- moveDocs.sh
+++ moveDocs.sh
@@ -1,10 +1,9 @@
 eslint built/*.js --fix > /dev/null&
-webpack&
 git submodule init
 git submodule update
 ts-node ClassGenerator.ts> /dev/null
-node_modules/.bin/typedoc --theme minimal -out tempDocs --tsconfig docsTsConfig.jsonc --ignoreCompilerErrors --mode modules --includeDeclarations --excludeExternals
-rm -rf docs/
-mv  tempDocs/ docs/
-node_modules/.bin/typedoc -out tempMarkdownDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules --theme markdown --includeDeclarations --excludeExternals
-mv  tempMarkdownDocs/ docs/markdown/
+# node_modules/.bin/typedoc --theme minimal -out tempDocs --tsconfig docsTsConfig.jsonc --ignoreCompilerErrors --mode modules --includeDeclarations --excludeExternals
+# rm -rf docs/
+# mv  tempDocs/ docs/
+# node_modules/.bin/typedoc -out tempMarkdownDocs --tsconfig tsconfig.json --ignoreCompilerErrors --mode modules --theme markdown --includeDeclarations --excludeExternals
+# mv  tempMarkdownDocs/ docs/markdown/
